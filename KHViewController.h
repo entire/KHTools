621a9d1fe4b99eaa3d0cb5df8f0a2ead1ba9767c
Kosuke Hata <hata.kosuke@gmail.com>
2013-12-11 20:42:04 -0800

    initial commit

---
+++ KHViewController.h
@@ -0,0 +1,20 @@
+//
+//  KHViewController.h
+//  blackcard
+//
+//  Created by Kosuke Hata on 9/18/13.
+//  Copyright (c) 2013 topiary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@class KHButton;
+
+@interface KHViewController : UIViewController
+
+@property (nonatomic) float width;
+@property (nonatomic) float height;
+@property (nonatomic) float y_start;
+@property (nonatomic) float x_start;
+@property (nonatomic) float barSize;
+
+@end
